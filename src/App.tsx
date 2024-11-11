a3332d1fe73d437a427ba8c329a46b278508bf65
Erik Karlsson <erikmatfors@gmail.com>
2024-11-11 18:30:02 +0100

    Routing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 // src/App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Booking from './components/Booking';
 import Confirmation from './components/Confirmation';
-import Menu from './components/Menu';
+import Home from './components/Home';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div>
-        <nav>
-          <Link to="/booking">Booking</Link>
-          <Link to="/confirmation">Confirmation</Link>
-          <Menu />
-        </nav>
-        <Routes>
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/confirmation" element={<Confirmation />} />
+      </Routes>
     </Router>
   );
 };
