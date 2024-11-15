46239857da365a5dbd114cfee060b3e34f1a5470
Erik Karlsson <erikmatfors@gmail.com>
2024-11-15 09:56:45 +0100

    Some comments removal

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,35 +1,3 @@
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-
-// const Menu: React.FC = () => {
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <div>
-//       {/* Hamburger-knapp */}
-//       <button className="hamburger" onClick={toggleMenu}>
-//         {/* En enkel hamburger-ikon med tre linjer */}
-//         <span className="line"></span>
-//         <span className="line"></span>
-//         <span className="line"></span>
-//       </button>
-
-//       {/* Menylänkar */}
-//       {isOpen && (
-//         <div className="menu">
-//           <Link to="/booking" onClick={toggleMenu}>Booking</Link>
-//           <Link to="/confirmation" onClick={toggleMenu}>Confirmation</Link>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Menu;
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
